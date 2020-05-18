590c8f306dd9315b9b6df4dde95dcfbbecf7c315
Szymon <szymon.czyzmanski@gmail.com>
2020-05-18 16:51:48 +0200

    Added input module

--- src/cube_main.c
+++ src/cube_main.c
@@ -1,29 +1,4 @@
-#include <stdio.h>
-
-#include "command.h"
-
-void read_data(char walls[][SIZE][SIZE]) {
-    char front = 0;
-    int layers = 0;
-    char angle = 0;
-
-    int c = getchar();
-    while (c != '.') {
-        if (c == '\n') {
-            execute_when_newline(walls, &front, &layers, &angle);
-        }
-        else if ('a' <= c && c <= 'z') {
-            execute_when_letter(walls, c, &front, &layers, &angle);
-        }
-        else if ('0' <= c && c <= '9') {
-            layers = execute_when_digit(c, layers);
-        }
-        else {
-            execute_when_quote(walls, c, &front, &layers, &angle);
-        }
-        c = getchar();
-    }
-}
+#include "input.h"
 
 int main() {
     char walls[6][SIZE][SIZE];
